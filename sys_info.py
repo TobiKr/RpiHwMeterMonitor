96a032ea74928ed181d8ca11a8c3eb83130f3aab
Tobias Kritten <tk@cloudguard.net>
2022-11-12 11:09:36 +0100

    Added energy meter / current date+time

--- sys_info.py
+++ sys_info.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Display basic system information.
+Display basic system and energy meter information.
 - host: nodename kernel-release (same as `uname -nr`)
 - ip: IP address
-- wifi: signal strength (RSSI) [dBm]
 - cpu: mean CPU load [%], calulcated as 1 min mean load / number of cores * 100
 - mem: memory usage [%], calculated as (total - available) / total * 100
 - disk: disk usage [%]
 - temp: CPU temperature [°C]
+- meter1/2: Energy Meter from IOBroker in Kilowatt/hour
+- datetimenow: current date and time 
 """
 
 import os
@@ -16,12 +17,12 @@ import time
 import psutil
 import socket
 import subprocess
+import json
+from datetime import datetime
 from PIL import ImageFont
 try:
-    from luma.emulator.device import pygame
-except ImportError:
-    from luma.core.interface.serial import i2c
-    from luma.oled.device import ssd1306
+    from luma.core.interface.serial import spi
+    from luma.lcd.device import st7735
 finally:
     from luma.core.render import canvas
 
@@ -32,7 +33,7 @@ def sinfo(key):
 
     host = lambda : '{0[1]} {0[2]}'.format(os.uname())
 
-    ip = lambda : socket.gethostbyname('%s.local'%socket.gethostname())
+    ip = lambda : 'IP: ' + socket.gethostbyname('%s.local'%socket.gethostname())
 
     wifi = lambda : subprocess.check_output(
         "/sbin/iwconfig wlan0 2>/dev/null | grep Signal | sed 's:.*=::'",
@@ -41,7 +42,7 @@ def sinfo(key):
 
     cpu = lambda : '%.0f%%'%(os.getloadavg()[0]/os.cpu_count()*100)
 
-    mem = lambda : 'MEM: %.0f%%'%psutil.virtual_memory().percent
+    mem = lambda : 'MEM: %.0f%%' %psutil.virtual_memory().percent
 
     disk = lambda : '%.0f%%'%psutil.disk_usage("/").percent
 
@@ -50,11 +51,24 @@ def sinfo(key):
         shell=True,
     ).decode('UTF-8')
 
+    meter1 = lambda: 'Z1 ' + str(getIoBrokerObjectState('0_userdata.0.Zaehler1_Test'))
+
+    meter2 = lambda: 'Z2 ' + str(getIoBrokerObjectState('0_userdata.0.Zaehler2_Test'))
+
+    datetimenow = lambda: datetime.now().strftime("%d.%m.%y %H:%M:%S")
+
     try:
-        return dict(host=host, ip=ip, wifi=wifi, cpu=cpu, mem=mem, disk=disk, temp=temp)[key]
+        return dict(host=host, ip=ip, wifi=wifi, cpu=cpu, mem=mem, disk=disk, temp=temp, meter1=meter1, meter2=meter2, datetimenow=datetimenow)[key]
     except KeyError:
         return None
 
+def getIoBrokerObjectState(object):
+    state = subprocess.check_output(
+        "/usr/bin/iobroker state get " + object,
+        shell=True,
+    ).decode('UTF-8')
+    state_dict = json.loads(state)
+    return state_dict['val']
 
 def stats(device, info, font):
     """
@@ -63,20 +77,17 @@ def stats(device, info, font):
     with canvas(device) as draw:
         for (x, y, icon, dx, dy, size, fn) in info:
             if icon:
-                draw.text((x, y), icon, font=font['icon_%s'%size], fill="white")
+                draw.text((x, y), icon, font=font['icon_%s'%size], fill="white", spacing=3)
             if fn:
                 x += dx; y += dy
-                draw.text((x, y), fn(), font=font['text_%s'%size], fill="white")
+                draw.text((x, y), fn(), font=font['text_%s'%size], fill="white", spacing=3)
 
 
 def main():
-    try:
-        serial = i2c(port=1, address=0x3C)
-        device = ssd1306(serial)
-        forever = True
-    except NameError:
-        device = pygame()
-        forever = False
+    s = spi(port=0, device=0, gpio_DC=23, gpio_RST=24)
+    device=st7735(s,rotate=2,width=160,height=128,h_offset=0,v_offset=0,bgr=False)
+    device.backlight(False)
+    forever = True
 
     # custom fonts
     font_path = "%s/fonts/%%s"%os.path.dirname(__file__)
@@ -89,20 +100,22 @@ def main():
 
     # layout: (x, y, icon, dx, dy, size, function)
     info = (
-        ( 1,  0, None    ,  0,  0, 'small', sinfo('host')),
-        ( 1, 15, None    ,  0,  0, 'small', sinfo('ip')),
-        ( 0, 29, '\uf2db', 22, -2, 'large', sinfo('cpu')),
-        ( 0, 50, None    ,  0,  0, 'small', sinfo('mem')),
-        (74, 15, '\uf1eb', 18,  0, 'small', sinfo('wifi')),
-        (76, 50, '\uf1c0', 13,  0, 'small', sinfo('disk')),
-        (78, 33, '\uf2c8', 10,  0, 'small', sinfo('temp')),
+        ( 0, 2, None     ,  0,  0, 'small', sinfo('ip')),
+        ( 0, 23, '\uf2db', 22,  0, 'small', sinfo('cpu')),
+        ( 0, 41, None    ,  0,  0, 'small', sinfo('mem')),
+        ( 2, 64, '\uf0e7', 12,  0, 'small', sinfo('meter1')),
+        #(95, 2, '\uf1eb' , 22,  0, 'small', sinfo('wifi')),
+        (87, 41, '\uf1c0', 22,  0, 'small', sinfo('disk')),
+        (89, 23, '\uf2c8', 22,  0, 'small', sinfo('temp')),
+        (89, 64, '\uf0e7', 12,  0, 'small', sinfo('meter2')),
+        ( 0,108, '\uf017', 18,  0, 'small', sinfo('datetimenow')),
     )
 
     stats(device, info, font)
-    time.sleep(15)  # 1st update after 15s
+    time.sleep(5)  # 1st update after 5s
     while forever:
         stats(device, info, font)
-        time.sleep(60) # update every minute
+        time.sleep(60) # update every 60 sec
 
 
 if __name__ == "__main__":
